52e0ee2e9a88398248d95172720a0be794a55b12
fwg <adf0001@163.com>
2022-10-07 12:11:35 +0800

    build git

---
+++ morgan-ym-d-log-stream.js
@@ -0,0 +1,42 @@
+﻿
+// morgan-ym-d-log-stream @ npm
+// A stream tool, mainly a plugin for morgan, to save log to file dir/YYYY/M/YYYY-MM-DD.log.
+
+var fs = require('fs');
+var path = require('path');
+
+module.exports = function (logDir) {
+	var logDate = -1;
+	var logStream = null;
+	var logFilePath = null;
+
+	return {
+		write: (str) => {
+			process.stdout.write(str);
+
+			var dtNow = new Date();
+			if (dtNow.getDate() !== logDate) {
+				if (logStream) {
+					logStream.close();
+					logStream = null;
+				}
+
+				var y = "" + dtNow.getFullYear();
+				var m = ("0" + (dtNow.getMonth() + 1)).slice(-2);
+				var d = ("0" + dtNow.getDate()).slice(-2);
+
+				var dir = path.join(logDir, y, m);
+				if (!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true });
+
+				logFilePath = path.join(dir, y + "-" + m + "-" + d + ".log");
+
+				logStream = fs.createWriteStream(logFilePath, { flags: 'a' });
+
+				logDate = dtNow.getDate();
+			}
+			logStream.write(str);
+		},
+
+		currentFilePath: () => logFilePath,
+	}
+}
---
+++ test/test-data.js
@@ -0,0 +1,44 @@
+
+//global variable, for html page, refer tpsvr @ npm.
+morgan_ym_d_log_stream = require("../morgan-ym-d-log-stream.js");
+
+module.exports = {
+
+	"morgan_ym_d_log_stream": function (done) {
+		if (typeof window !== "undefined") throw "disable for browser";
+
+		var path = require('path');
+
+		var logStream = morgan_ym_d_log_stream(path.join(__dirname, "log"));
+
+		logStream.write("test, " + (new Date()).toLocaleString() + "\n");
+
+		var fpath = logStream.currentFilePath()
+		console.log(fpath);
+
+		done(!(
+			fpath
+		));
+	},
+
+	"check exports": function (done) {
+		var m = morgan_ym_d_log_stream;
+		for (var i in m) {
+			if (typeof m[i] === "undefined") { done("undefined: " + i); return; }
+		}
+		done(false);
+
+		console.log(m);
+		var list = "export list: " + Object.keys(m).join(", ");
+		console.log(list);
+		return list;
+	},
+
+};
+
+// for html page
+//if (typeof setHtmlPage === "function") setHtmlPage("title", "10em", 1);	//page setting
+if (typeof showResult !== "function") showResult = function (text) { console.log(text); }
+
+//for mocha
+if (typeof describe === "function") describe('morgan_ym_d_log_stream', function () { for (var i in module.exports) { it(i, module.exports[i]).timeout(5000); } });
